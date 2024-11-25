8a9671f68b64506ae67071edb3ddeda2df97ad37
Hance <131955098+Hance08@users.noreply.github.com>
2024-11-26 00:01:25 +0800

    Summary
    update index.html
    update styles.css
    add a test location
    change the card bref info

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import secrets
-from flask import Flask, render_template, redirect, url_for, request, flash
 from werkzeug.utils import secure_filename
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
